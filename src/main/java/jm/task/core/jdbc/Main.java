2fe1db3210f5150f334d822f124740812f5495e0
RRA <radzhabovra93@yandex.ru>
2025-12-17 22:30:30 +0300

    initial commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,30 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Test1", "Test11", (byte) 25);
+        userService.saveUser("Test2", "Test22", (byte) 27);
+        userService.saveUser("Test3", "Test33", (byte) 31);
+        userService.saveUser("Test4", "Test44", (byte) 18);
+
+        List<User> allUsers = userService.getAllUsers();
+        for (User user: allUsers) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,27 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String LOGIN = "postgres";
+    private static final String PASSWORD = "postgres";
+
+//    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, LOGIN, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    public static void setConnection(Connection connection) {
+//        Util.connection = connection;
+//    }
+    // реализуйте настройку соеденения с БД
+}
